816c8d805cd2ac30a006c94e568803dd89999dff
Derk van der Heide <derkvanderheide@hotmail.com>
2022-09-29 19:43:08 +0200

    add commands config test

---
+++ tests/config/commands.test.ts
@@ -0,0 +1,19 @@
+import commands from '@config/commands';
+import {jest, test, expect} from '@jest/globals';
+
+test('All the required properties in commands config exists', () => {
+    commands.forEach(command => {
+        expect(command).toHaveProperty('name');
+        expect(command).toHaveProperty('description');
+        expect(command).toHaveProperty('requiredPermissions');
+        expect(command).toHaveProperty('ephemeral');
+        expect(command).toHaveProperty('arguments');
+
+        command.arguments.forEach(argument => {
+            expect(argument).toHaveProperty('name');
+            expect(argument).toHaveProperty('description');
+            expect(argument).toHaveProperty('required');
+            expect(argument).toHaveProperty('type');
+        });
+    });
+});
